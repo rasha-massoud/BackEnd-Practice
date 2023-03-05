cb6c2a34e6180bcc53193564f08bd21a72e86c22
RashaM <rashamassoud1998@gmail.com>
2023-03-06 01:37:45 +0200

    Added the ip address GET API to retrieve IP Address

---
+++ ipAddress.php
@@ -0,0 +1,20 @@
+<?php 
+include("connection.php"); 
+
+if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+}elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+}else{
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+
+$response["ip"]=$ip;
+
+echo json_encode($response);
+
+?>
+
+
+
+
