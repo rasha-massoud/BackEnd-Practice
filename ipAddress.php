11ecfcd26c9eaa7bf3ddd3292bf620f7c14bfc01
RashaM <rashamassoud1998@gmail.com>
2023-03-06 01:38:56 +0200

    Added another method to retrieve IP Address

--- ipAddress.php
+++ ipAddress.php
@@ -1,15 +1,11 @@
 <?php 
 include("connection.php"); 
+header("Content-Type: application/json");
+$ip = $_SERVER['REMOTE_ADDR'];
 
-if(!empty($_SERVER['HTTP_CLIENT_IP'])){
-    $ip = $_SERVER['HTTP_CLIENT_IP'];
-}elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
-    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-}else{
-    $ip = $_SERVER['REMOTE_ADDR'];
-}
-
-$response["ip"]=$ip;
+$response = array(
+    "ip" => $ip
+);
 
 echo json_encode($response);
 
