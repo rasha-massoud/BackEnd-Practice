d15d090f9b5208e3f8af44cdec408e9626c5e417
RashaM <rashamassoud1998@gmail.com>
2023-03-05 22:00:28 +0200

    Return a bool indicating the validation status of the email and password

--- formatValidate.php
+++ formatValidate.php
@@ -6,9 +6,11 @@ $password=$_POST['password'];
 
 if(filter_var($mail, FILTER_VALIDATE_EMAIL)){
     $response["mail_status"]="The Email Format is Valid!";
+    $MAIL=true;
 }
 else{
     $response["mail_status"]="Sorry! Invalid Email Format!";
+    $MAIL=false;
 }
 
 // Validate password strength
@@ -19,10 +21,19 @@ $specialChars = preg_match('@[^\w]@', $password);
 
 if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
     $response["password_status"]="Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.";
+    $PASS=false;
 }else{
     $response["password_status"]="Strong password.";
+    $PASS=true;
 }
 
-echo json_encode($response);
+if($MAIL && $PASS){
+    $response["status"]=true;
+}
+else{
+    $response["status"]=false;
+}
+
+echo json_encode($response["status"]);
 
 ?>
